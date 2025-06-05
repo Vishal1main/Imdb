8add6422eb9d3fd806584e98183cc906cbda2e5a
Vishal1main <monsterr258258@gmail.com>
2025-06-05 19:36:40 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { Telegraf } = require('telegraf');
+const { Telegraf, Markup } = require('telegraf');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 const app = express();
@@ -7,11 +7,11 @@ const app = express();
 // Telegram Bot Code
 bot.start((ctx) => {
   const user = ctx.from;
-  const nameWithLink = `<a href="tg://user?id=${user.id}">${user.first_name}</a>`;
 
-  ctx.replyWithHTML(
-    `<b>Hey 👋 ${nameWithLink}</b>\n<b>Welcome To My About Bot 😎</b>\n\n<b>• In This Bot Have Some Info About Me</b>`,
+  ctx.reply(
+    `<b>Hey 👋 ${user.first_name} Welcome To My About Bot 😎\n\n• In This Bot Have Some Info About Me</b>`,
     {
+      parse_mode: "HTML",
       disable_web_page_preview: true,
       reply_markup: {
         inline_keyboard: [
@@ -31,9 +31,7 @@ bot.start((ctx) => {
 
 bot.action("channels", (ctx) => {
   ctx.editMessageText(
-    `<b>📢 My Channels:</b>\n\n` +
-    `<b>🎯 SkyHub4u</b> - <a href="https://t.me/Sky_hub4u">Click</a>\n` +
-    `<b>🎯 Tmrbotz</b> - <a href="https://t.me/Tmr_Botz">Click</a>`,
+    `<b>📢 My Channels:\n\n🎯 SkyHub4u <a href="https://t.me/Sky_hub4u">Click</a>\n🎯 Tmrbotz <a href="https://t.me/Tmr_Botz">Click</a></b>`,
     {
       parse_mode: "HTML",
       disable_web_page_preview: true,
@@ -46,12 +44,7 @@ bot.action("channels", (ctx) => {
 
 bot.action("mybots", (ctx) => {
   ctx.editMessageText(
-    `<b>🤖 My Bots:</b>\n\n` +
-    `<b>🤖 Mʀ sᴇɴᴅᴇʀ™</b> - <a href="https://t.me/Mr_Movie_Sender_Bot">Click</a>\n` +
-    `<b>🤖 Pᴜsʜᴘᴀ ʙᴏᴛ™</b> - <a href="https://t.me/Pushpa_Moviee_bot">Click</a>\n` +
-    `<b>🤖 Aʟᴘʜᴀ ᴍᴏᴠɪᴇ ʙᴏᴛ™</b> - <a href="https://t.me/Alphaa_Movie_Bot">Click</a>\n` +
-    `<b>🤖 Tmr Spotify Bot</b> - <a href="https://t.me/Tmr_Spotify_Bot">Click</a>\n` +
-    `<b>🤖 SkyHub Game 🎮</b> - <a href="https://t.me/SkyhubGame_Bot">Click</a>`,
+    `<b>🤖 My Bots:\n\n🤖 Mʀ sᴇɴᴅᴇʀ™ <a href="https://t.me/Mr_Movie_Sender_Bot">Click</a>\n🤖 Pᴜsʜᴘᴀ ʙᴏᴛ™ <a href="https://t.me/Pushpa_Moviee_bot">Click</a>\n🤖 Aʟᴘʜᴀ ᴍᴏᴠɪᴇ ʙᴏᴛ™ <a href="https://t.me/Alphaa_Movie_Bot">Click</a>\n🤖 Tmr Spotify Bot <a href="https://t.me/Tmr_Spotify_Bot">Click</a>\n🤖 SkyHub Game 🎮 <a href="https://t.me/SkyhubGame_Bot">Click</a></b>`,
     {
       parse_mode: "HTML",
       disable_web_page_preview: true,
@@ -64,14 +57,7 @@ bot.action("mybots", (ctx) => {
 
 bot.action("myid", (ctx) => {
   ctx.editMessageText(
-    `<b>👤 My Info:</b>\n` +
-    `<b>━━━━━━━━━━➣</b>\n` +
-    `<b>┣⬡ ɴᴀᴍᴇ :</b> Vishal\n` +
-    `<b>┣⬡ ᴀɢᴇ :</b> 18+\n` +
-    `<b>┣⬡ ɢᴇɴᴅᴇʀ :</b> ᴍᴀʟᴇ\n` +
-    `<b>┣⬡ ᴩʟᴀᴄᴇ :</b> Iɴᴅɪᴀ 🇮🇳\n` +
-    `<b>┣⬡ ʟᴀɴɢᴜᴀɢᴇ :</b> Hindi\n` +
-    `<b>┣⬡ ꜱᴛᴜᴅy ɪɴ :</b> ㅤㅤ`,
+    `<b>👤 My Info:\n━━━━━━━━━━➣\n┣⬡ ɴᴀᴍᴇ : Vishal\n┣⬡ ᴀɢᴇ : 18+\n┣⬡ ɢᴇɴᴅᴇʀ : ᴍᴀʟᴇ\n┣⬡ ᴩʟᴀᴄᴇ : Iɴᴅɪᴀ 🇮🇳\n┣⬡ ʟᴀɴɢᴜᴀɢᴇ : Hindi\n┣⬡ ꜱᴛᴜᴅy ɪɴ : ㅤㅤ</b>`,
     {
       parse_mode: "HTML",
       disable_web_page_preview: true,
@@ -84,9 +70,7 @@ bot.action("myid", (ctx) => {
 
 bot.action("contact", (ctx) => {
   ctx.editMessageText(
-    `<b>💸 For Paid Promotion:</b>\n\n` +
-    `<b>Dm Me For 💸 Paid Promotion</b>\n` +
-    `<b>📱 Telegram:</b> @Tmr_Developer`,
+    `<b>💸 For Paid Promotion:\n\nDm Me For 💸 Paid Promotion\n📱 Telegram: @Tmr_Developer</b>`,
     {
       parse_mode: "HTML",
       disable_web_page_preview: true,
@@ -99,10 +83,9 @@ bot.action("contact", (ctx) => {
 
 bot.action("back", (ctx) => {
   const user = ctx.from;
-  const nameWithLink = `<a href="tg://user?id=${user.id}">${user.first_name}</a>`;
 
   ctx.editMessageText(
-    `<b>Hey 👋 ${nameWithLink}</b>\n<b>Welcome To My About Bot 😎</b>\n\n<b>• In This Bot Have Some Info About Me</b>`,
+    `<b>Hey 👋 ${user.first_name} Welcome To My About Bot 😎\n\n• In This Bot Have Some Info About Me</b>`,
     {
       parse_mode: "HTML",
       disable_web_page_preview: true,
@@ -125,7 +108,7 @@ bot.action("back", (ctx) => {
 // Launch bot
 bot.launch();
 
-// Webhook support for Render
+// Webhook support for Render (don't hardcode port!)
 const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => res.send("Bot is Running!"));
 app.listen(PORT, () => {
